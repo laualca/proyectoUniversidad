597fe66c7319bfc1cdd2b52ece593b45e5d4ec96
laualca <lmalvarez1@uth.hn>
2024-07-29 16:25:23 -0600

    tabla Carrera commit

--- Universidad/Modulos/Academica/models.py
+++ Universidad/Modulos/Academica/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Carrera(models.Model):
+    codigo = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=100)
+    duracion = models.PositiveSmallIntegerField(default=8)
+    
+
+    
