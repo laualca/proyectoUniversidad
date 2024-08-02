91bda80f58d197c751bfd3f87cf79d53165c896d
laualca <lmalvarez1@uth.hn>
2024-08-01 20:46:59 -0600

    commit 1 de agosto 2024

--- Universidad/Modulos/Academica/apps.py
+++ Universidad/Modulos/Academica/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AcademicaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Academica'
+    name = 'Modulos.Academica'
--- Universidad/Modulos/Academica/models.py
+++ Universidad/Modulos/Academica/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
 # Create your models here.
-# creacion de la clase Carrera de la tabla carrera
+#MODELO creacion de la clase Carrera de la tabla carrera
 class Carrera(models.Model):
     codigoCarrera = models.CharField(max_length=10, primary_key=True)
     nombre = models.CharField(max_length=100)
     duracion = models.PositiveSmallIntegerField(default=8)
+    
+    def __str__(self):
+        txt = "{0} (Duracion: {1} año(s))"
+        return txt.format(self.nombre, self.duracion)
 
-#creacion de la clase Estudiante de la tabla estudiante    
+#MODELO creacion de la clase Estudiante de la tabla estudiante  
 class Estudiante(models.Model):
-    codigoEstudiante = models.CharField(max_length=15, primary_key=True)
+    codigoEstudiante = models.AutoField(primary_key=True)
     dni = models.CharField(max_length=15)
     nombres = models.CharField(max_length=100)
     apellidoPaterno = models.CharField(max_length=100)
@@ -29,3 +33,39 @@ class Estudiante(models.Model):
     def nombreCompleto(self):
         txt = "{0} {1}, {2}" 
         return txt.format(self.apellidoPaterno, self.apellidoMaterno, self.nombres)
+    
+    def __str__(self):
+        txt = "{0} / Carrera: {1} / {2}"
+        if self.vigencia:
+            estadoEstudiante = "VIGENTE"
+        else:
+            estadoEstudiante = "RETIRADO"
+        return txt.format(self.nombreCompleto(), self.carrera, estadoEstudiante)
+    
+#MODELO creacion de la clase Curso de la tabla curso
+class Curso(models.Model):
+    codigoCurso = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=100)
+    creditos = models.PositiveSmallIntegerField(default=5)
+    docente = models.CharField(max_length=100)
+    
+    def __str__(self):
+        txt = "{0} ({1}) / Docente: {2}"
+        return txt.format(self.nombre, self.codigoCurso, self.docente)
+    
+#MODELO creacion de la clase Matricula de la tabla matricula
+class Matricula(models.Model):
+    id = models.AutoField(primary_key=True)
+    fechaMatricula = models.DateTimeField(auto_now_add=True)
+    #relacion de la tabla matricula con la tabla estudiante llave foranea
+    estudiante = models.ForeignKey(Estudiante, null=False, blank=False, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, null=False, blank=False, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        txt = "{0} matriculad{1} en el curso {2} / Fecha: {3}"
+        if self.estudiante.sexo == 'F':
+            letraSexo = "a"
+        else:
+            letraSexo = "o"
+        fecMat = self.fechaMatricula.strftime("%A %d/%m/%Y %H:%M:%S")
+        return txt.format(self.estudiante.nombreCompleto(), letraSexo, self.curso, fecMat)
